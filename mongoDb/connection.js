babfaab8ccf7b6d3e5f9f56bccea1b436197b7c9
kasper879 <kasper8791@gmail.com>
2017-11-08 11:14:00 +0100

    d

--- mongoDb/connection.js
+++ mongoDb/connection.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var config = require('../config')
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://${config.keys.userName}:<${config.keys.password}>@quotedb-shard-00-00-xmbu1.mongodb.net:27017,quotedb-shard-00-01-xmbu1.mongodb.net:27017,quotedb-shard-00-02-xmbu1.mongodb.net:27017/test?ssl=true&replicaSet=QuoteDB-shard-0&authSource=admin` , { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || `mongodb://${config.keys.userName}:${config.keys.password}@quotedb-shard-00-00-xmbu1.mongodb.net:27017,quotedb-shard-00-01-xmbu1.mongodb.net:27017,quotedb-shard-00-02-xmbu1.mongodb.net:27017/test?ssl=true&replicaSet=QuoteDB-shard-0&authSource=admin` , { useMongoClient: true });
 
 
 module.exports = { mongoose };
