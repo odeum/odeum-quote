a623833883da8e6705b92b190dd5e73515e3932d
kasper879 <kasper8791@gmail.com>
2017-11-08 13:05:18 +0100

    comit

---
+++ Routes/productRoute.js
@@ -0,0 +1,11 @@
+
+var express = require("express");
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var router = express.Router();
+
+module.exports = router; 
+
+router.get('/products', () => {
+    
+})
--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var { mongoose } = require('./MongoDb/connection');
+var productRoute = require('./Routes/productRoute'); 
 
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+
+app.use('', productRoute);
 
 
 app.listen(port, () => {
