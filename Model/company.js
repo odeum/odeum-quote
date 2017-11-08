a401fcb4cfa550898b539740a97a3bb76b992155
Frida Jakobsen <1054351@ucn.dk>
2017-11-08 15:04:56 +0100

    compModel

--- Model/company.js
+++ Model/company.js
@@ -1,7 +1,31 @@
 var mongoose = require('mongoose'); 
 
 var Company = { 
-    name: {
+    companyName: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    contactPerson: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    email: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    phone: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    address: {
         type: String, 
         required: true, 
         trim: true,
