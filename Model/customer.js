764c972827627c614e6f406084e435b94eb9ded3
Frida Jakobsen <1054351@ucn.dk>
2017-11-09 08:56:37 +0100

    company deleted

--- Model/customer.js
+++ Model/customer.js
@@ -1,7 +1,34 @@
-var mongoose = require('mongoose'); 
-
-var {Company} = require('./company'); 
+var mongoose = require('mongoose');
 
 var Customer = mongoose.model('Customer', {
-    Company
+    companyName: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    contactPerson: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    email: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    phone: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    address: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    }
 }); 
--- Model/salesPerson.js
+++ Model/salesPerson.js
@@ -1,7 +1,34 @@
 var mongoose = require('mongoose'); 
 
-var {Company} = require('./company'); 
-
 var SalesPerson = mongoose.model('SalesPerson', {
-    Company
+    companyName: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    contactPerson: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    email: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    phone: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    },
+    address: {
+        type: String, 
+        required: true, 
+        trim: true,
+        minlength: 1
+    }
 }); 
