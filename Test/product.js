545bc1f096e84e88b2f79ca90d9fcdcc56b3570d
lichondk <lichondk@gmail.com>
2017-11-15 15:14:08 +0100

    testing

--- Test/product.js
+++ Test/product.js
@@ -1,24 +1,21 @@
-process.env.NODE_ENV = 'test'
-
 var monogoose = require("mongoose");
 var product = require('../Model/product');
-var test = require('./models');
+var allProducts = require('./models');
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var app = require('../app');
 var should = chai.should();
-
 chai.use(chaiHttp);
 
 describe('/Get Product', ()=> {
     it('it should GET all produckts', (done) => {
-        chai.request('http://localhost:3000/product/')
-        .get('products/')
+        chai.request(app)
+        .get('/product/products')
         .end((err, res) => {
             res.should.have.status(200);
             res.body.should.be.a('object');
-            res.body.should.be.eql(test)
+            res.body.should.be.eql(allProducts)
         done();
         })
     })
