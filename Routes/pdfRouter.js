1ef21a6220d806e99209eb7e9fd3b23916868754
fruja <frida.jakobsen@hotmail.com>
2017-12-14 12:37:04 +0100

    PDF comments and font added

--- Routes/pdfRouter.js
+++ Routes/pdfRouter.js
@@ -4,12 +4,11 @@ var router = express.Router();
 var sendMail = require('../CTR/controller')
 var { createPdf } = require('../CTR/pdfMake')
 
-
-
 module.exports = router;
 
 router.post('/post/:name/:email', (req, res) => {
     var date = req.body.date
+
     var companyName = req.body.customerName
     var customerFirstName = req.body.customerContactFirstName
     var customerEmail = req.body.customerEmail
@@ -24,20 +23,21 @@ router.post('/post/:name/:email', (req, res) => {
     var companyContactName = req.body.contactPerson
     var companyEmail = req.body.email
     var companyPhone = req.body.phone
+
     var products = req.body.product
     var totalPrice = req.body.totalPrice
+
     createPdf(date, companyName, customerEmail, customerFirstName, customerLastName, customerAdress,
         customerZip, customerCity, salesPersonName, companyContactName, companyEmail, companyPhone, products, totalPrice, description, function(){
             sendMail(req.params.name, req.params.email)
         })
-
-
     res.send('der er sent en pdf');
 })
 
 
 router.post('/postPdf/', (req, res) => {
     var date = req.body.date
+
     var companyName = req.body.customerName
     var customerFirstName = req.body.customerContactFirstName
     var customerLastName = req.body.customerContactLastName
@@ -54,14 +54,8 @@ router.post('/postPdf/', (req, res) => {
 
     var products = req.body.product
     var totalPrice = req.body.totalPrice
-    /*
-    kunde: virksomhenavn, concatcPerson,emaifls, tlf
-    date: 
-    webHouse: navn, concatkperson, email, phone
-    produkt: navn, beskrivelse, price. totalpris 
-    */
-    //console.log('req', req.body)
+    
     createPdf(date, companyName, customerFirstName, customerLastName, customerAdress,
         customerZip, customerCity, salesPersonName, companyContactName, companyEmail, companyPhone, products, totalPrice, description);
-    res.send('kuku');
+    res.send('PDF filen er oprettet');
 })
