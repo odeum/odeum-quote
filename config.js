caae7885a71d124794e076d459ef690b225c572d
unknown <kapser8791@gmail.com>
2018-01-11 19:51:26 +0100

    git comit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports= {
+    keys: {
+        userName:'franka',
+        password:'Franka_1'
+
+    }, 
+    mail:{
+        service: 'gmail',
+        mail: 'odeum.quote1@gmail.com',
+        password: 'quote123'
+    }
+}
