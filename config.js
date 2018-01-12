e55ddb41262e8f82010d2b5e0fab4658316ace0c
kasper879 <kasper8791@gmail.com>
2018-01-12 09:23:45 +0100

    hallo

---
+++ config.js
@@ -0,0 +1,14 @@
+
+module.exports= {
+    keys: {
+        userName:'franka',
+        password:'Franka_1'
+
+    }, 
+    mail:{
+        service: 'gmail',
+        mail: 'odeum.quote1@gmail.com',
+        password: 'quote123'
+    }
+}
+
